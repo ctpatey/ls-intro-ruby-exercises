31190c3bb8a2f7a392905b02c8b0472a7d58943b
ctpatey <ctpatey@gmail.com>
2019-07-16 17:26:01 -0400

    complete exercises 6 and 7

---
+++ the_basics/exercise6.rb
@@ -0,0 +1,10 @@
+# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+
+def square(number)
+
+	puts number ** 2
+end
+
+square(2)
+square(3)
+square(4)
---
+++ the_basics/exercise7.rb
@@ -0,0 +1,7 @@
+# hat does the following error message tell you?
+
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+#   from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+
+# There is a bracket pair without a closing bracket, like a block is missing the '}' or it's the wrong bracket. The problem is on line 2.  
